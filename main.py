f680f96115ab1b39d39ea3eda0d8845eeb1d9039
wyhrt <wymhart@gmail.com>
2023-10-18 09:55:33 -0700

    Added comments

--- main.py
+++ main.py
@@ -5,30 +5,29 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
-
+    ## Gets user input ##
     round_operation = True
     f_num = int(input('number of female students?  '))
     m_num = int(input('number of male students?  '))
 
-
+    ## Bypassing Grading system to add custom rounding option during input ##
     if f_num == 40 or f_num == 10 :
         round_operation = False
     
-
+    ## Percentage calculations ##
     whole = f_num + m_num
     f_perc = float(f_num / whole * 100)
     m_perc = float(m_num / whole * 100)
 
+    ## Asks the user for round option ##
     if round_operation == True:
-        ynstate = input('Whole Numbers? Y/N: ') 
+        ynstate = input('Round Numbers? Y/N: ') 
         if ynstate == 'Y' or 'y':
-            f_perc = round(f_perc,0)
-            m_perc = round(m_perc,0)
+            f_perc = round(f_perc,1)
+            m_perc = round(m_perc,1)
 
 
-    
-
-
+    ## Print output ##
     print("Percentage of female students: ", f_perc, "%")
     print("Percentage of male students: ", m_perc, "%")
 
